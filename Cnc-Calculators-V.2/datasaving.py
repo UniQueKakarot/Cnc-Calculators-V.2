4440f117bec4313d06183d79213b77fabc138c02
UniQueKakarot <iverullrik1992@gmail.com>
2017-12-23 16:59:24 +0100

    .

---
+++ Cnc-Calculators-V.2/datasaving.py
@@ -0,0 +1,13 @@
+import openpyxl as pyxl
+
+
+class CuttingSpeedData():
+    def __init__(self, cutspd, milld, noft, fpt)
+    
+    self.cutspd = cutspd
+    self.milld = milld
+    self.noft = noft
+    self.fpt = fpt
+    
+    def filegen(self):
+        pass
