73af58a504291da48a085b2eb20bf9b4237fd307
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2018-02-13 12:39:20 +0100

    Fixed error handling for straigth toolpath angle

--- Cnc-Calculators-V.2/Moduler/straight.py
+++ Cnc-Calculators-V.2/Moduler/straight.py
@@ -7,10 +7,10 @@ from Moduler.customwidgets import MyLabel
 
 Builder.load_string(
     """
-    
+
 <BoxLayout>:
     orientation: 'horizontal'
-    
+
 <Label>:
     font_size: 20
 
@@ -21,11 +21,11 @@ Builder.load_string(
 
     tpl: tpl
     zstep: zstep
-    
+
     cols: 1
     padding: 10
     spacing: 10
-    
+
     BoxLayout:
         size_hint_y: None
         height: "40dp"
@@ -36,7 +36,8 @@ Builder.load_string(
             hint_text: "mm"
             multiline: False
             write_tab: False
-            
+            on_text_validate: root.calc()
+
     BoxLayout:
         size_hint_y: None
         height: "40dp"
@@ -47,16 +48,17 @@ Builder.load_string(
             hint_text: "Z Step"
             multiline: False
             write_tab: False
-            
+            on_text_validate: root.calc()
+
     BoxLayout:
         size_hint_y: None
         height: "40dp"
         Button:
             text: "Calculate"
             on_press: root.calc()
-            
+
     Label:
-            
+
     BoxLayout:
         size_hint_y: None
         height: "40dp"
@@ -64,47 +66,46 @@ Builder.load_string(
             text: "Angle:"
             font_size: 20
             bcolor: [1, 1, 1, 0.15]
-            
+
         MyLabel:
             text: root.angle
             font_size: 20
             bcolor: [1, 1, 1, 0.15]
-    
+
     """
 )
 
 class Straight(GridLayout):
-    
+
     angle = StringProperty()
-    
+
     def calc(self):
-        
+
         """ Doing the mathy thingy """
-        
+
         try:
             tpl = self.tpl.text
             tpl = tpl.replace(',', '.')
             tpl = float(tpl)
         except ValueError:
-            tpl = 0
-            self.angle = "Please input values"
-        
-        try:    
+            pass
+
+        try:
             zstep = self.zstep.text
             zstep = zstep.replace(',', '.')
             zstep = float(zstep)
         except ValueError:
-            zstep = 0
-            self.angle = "Please input values"
-        try:    
-            result = (sin(1.570796) * zstep) / tpl
-        except ZeroDivisionError:
-            self.angle = "Don't divide by zero you naughty boi"
-            
-        
-        result = degrees(result)
-        result = round(result, 2)
-        
-        self.angle = str(result)
+            pass
 
-    
+        try:
+            result = (sin(1.570796) * zstep) / tpl
+            result = degrees(result)
+            result = round(result, 2)
+        except (ZeroDivisionError, TypeError):
+            result = "Please input values"
+            pass
+
+
+
+
+        self.angle = str(result)
