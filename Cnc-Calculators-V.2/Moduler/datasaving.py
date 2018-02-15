d930a0117abc7cd694455ab2c670f2e98f4ed304
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2018-02-15 17:27:36 +0100

    Started on the datasaving class for MRR

--- Cnc-Calculators-V.2/Moduler/datasaving.py
+++ Cnc-Calculators-V.2/Moduler/datasaving.py
@@ -83,3 +83,33 @@ class CuttingSpeedData():
         ws.cell(row=row, column=5, value=tooth)
 
         self.wb.save(self.data)
+
+
+class MaterialRemovalData():
+
+    def __init__(self, data):
+        """ Checking if the xlsx file has all the necessities,
+            and if not generate it """
+
+        self.data = data
+
+        self.wb = load_workbook(self.data)
+
+        test = 0
+
+        for i in self.wb.get_sheet_names():
+            if i == "Material Removal Rate":
+                test = 1
+
+        if test == 0:
+            self.ws = self.wb.create_sheet("Cutting Speed")
+            x = self.wb.get_sheet_by_name('Sheet')
+            self.wb.remove_sheet(x)
+
+            ws = self.wb["Cutting Speed"]
+            ws.cell(row=2, column=2, value="Cutting Meter")
+            ws.cell(row=2, column=3, value="Mill Diameter")
+            ws.cell(row=2, column=4, value="Number of teeth")
+            ws.cell(row=2, column=5, value="Feed pr Tooth")
+
+        self.wb.save(self.data)
