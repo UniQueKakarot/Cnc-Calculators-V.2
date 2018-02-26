d4afc742274481c9f3952c89da779a1ef3c6eea7
UniQueKakarot <iverullrik92@gmail.com>
2018-02-26 21:02:00 +0100

    .

--- Cnc-Calculators-V.2/Moduler/trigonometry.py
+++ Cnc-Calculators-V.2/Moduler/trigonometry.py
@@ -1,8 +1,11 @@
 """ This module contains a Trigonometry calculator """
 
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty
+
+from Moduler.customwidgets import MyLabel
+from Moduler.customwidgets import MyTextInput
 
 Builder.load_string(
     """
@@ -11,16 +14,55 @@ Builder.load_string(
         Line:
             points: self.points
 
+    BoxLayout:
+        size_hint_y: None
+        height: "40dp"
+        size_hint_x: 0.3
+        pos: 0, 480
+        Label:
+            text: "Angle A"
+
+        MyTextInput:
+            hint_text:
+            multiline: False
+            write_tab: False
+            on_text_validate:
+
+    BoxLayout:
+        size_hint_y: None
+        height: "40dp"
+        size_hint_x: 0.3
+        pos: 0, 430
+        Label:
+            text: "Angle B"
+
+        MyTextInput:
+            hint_text:
+            multiline: False
+            write_tab: False
+            on_text_validate:
+
+    BoxLayout:
+        size_hint_y: None
+        height: "40dp"
+        size_hint_x: 0.3
+        pos: 0, 380
+        Label:
+            text: "Side A"
+
+        MyTextInput:
+            hint_text:
+            multiline: False
+            write_tab: False
+            on_text_validate:
+
+
     #Button:
     """
 )
 
 
-
-
-
-
-class Trigonometry(BoxLayout):
+class Trigonometry(FloatLayout):
 
     points = ListProperty([(100, 100), (200, 100), (200, 200), (100, 100)])
 
