3fdb923816b513385da1a287ad71a6690ba36363
UniQueKakarot <iverullrik92@gmail.com>
2018-03-02 23:14:39 +0100

    made a moving triangle

--- Cnc-Calculators-V.2/Moduler/trigonometry.py
+++ Cnc-Calculators-V.2/Moduler/trigonometry.py
@@ -1,5 +1,7 @@
 """ This module contains a Trigonometry calculator """
 
+from math import sin, degrees, radians
+
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty
@@ -10,6 +12,11 @@ from Moduler.customwidgets import MyTextInput
 Builder.load_string(
     """
 <Trigonometry>:
+
+    aa: aa
+    ac: ac
+    sa: sa
+
     canvas:
         Line:
             points: self.points
@@ -23,6 +30,7 @@ Builder.load_string(
             text: "Angle A"
 
         MyTextInput:
+            id: aa
             hint_text:
             multiline: False
             write_tab: False
@@ -34,9 +42,10 @@ Builder.load_string(
         size_hint_x: 0.3
         pos: 0, 430
         Label:
-            text: "Angle B"
+            text: "Angle C"
 
         MyTextInput:
+            id: ac
             hint_text:
             multiline: False
             write_tab: False
@@ -51,19 +60,67 @@ Builder.load_string(
             text: "Side A"
 
         MyTextInput:
+            id: sa
             hint_text:
             multiline: False
             write_tab: False
             on_text_validate:
 
-
-    #Button:
+    BoxLayout:
+        size_hint_y: None
+        height: "40dp"
+        size_hint_x: 0.3
+        pos: 450, 380
+        Button:
+            text: "Run"
+            on_press: root.test()
     """
 )
 
 
 class Trigonometry(FloatLayout):
 
-    points = ListProperty([(100, 100), (200, 100), (200, 200), (100, 100)])
+    points = ListProperty()
 
-    pass
+    def test(self):
+
+        # converting angle a from text to int
+        angle_a = self.aa.text
+        angle_a = int(angle_a)
+
+        # converting angle c from text to int
+        angle_c = self.ac.text
+        angle_c = int(angle_c)
+
+        # converting the length of side a from text to int
+        side_a = self.sa.text
+        side_a = int(side_a)
+
+        # calculating angle b from whats left
+        angle_b = 180 - angle_a - angle_c
+
+        # python sure loves it radians
+        angle_a = radians(angle_a)
+        angle_c = radians(angle_c)
+        angle_b = radians(angle_b)
+
+        # doing the math for the opposite
+        opposite = (sin(angle_a) * side_a) / sin(angle_b) 
+
+        # doing the math for the hypotenuse
+        hypotenuse = (sin(angle_c) * side_a) / sin(angle_b)
+
+        print(angle_b, '\n')
+        print(opposite, '\n')
+        print(hypotenuse)
+
+        test = 100 + opposite
+
+        # assigning my listproperty some values to draw a triangle
+        self.points = [(100, 100), (400, 100), (400, test), (100, 100)]
+
+        #TODO
+        # made some headway on drawing the triangle after the calculations
+        # so now i have to decide if i want the length of the triangle
+        # to represent the drawn triangle, which might be a cool thing to do
+        # position needs more consideration and how do we present the results?
