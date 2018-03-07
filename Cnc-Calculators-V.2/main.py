9c22216e1b683830477a1bd11b2d2c5792215a9f
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2018-03-07 09:48:39 +0100

    .

--- Cnc-Calculators-V.2/main.py
+++ Cnc-Calculators-V.2/main.py
@@ -22,7 +22,7 @@ class MainBody(BoxLayout):
 class CncCalculators(App):
     """ Root class for the GUI application """
     def build(self):
-        self.load_kv("CncCalculators.kv")
+        #self.load_kv("CncCalculators.kv")
         return MainBody()
 
 if __name__ == "__main__":
